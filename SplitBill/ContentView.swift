3fe88d621f5e161875f5301a2f55470622782598
R. Kukuh <rkukuh@gmail.com>
2024-10-11 03:37:20 +0700

    Make it simplest

--- SplitBill/ContentView.swift
+++ SplitBill/ContentView.swift
@@ -8,11 +8,6 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var name: String = ""
-    @State private var email: String = ""
-    @State private var age: Int = 18
-    @State private var receiveNewsletter: Bool = false
-    
     var body: some View {
         Form {
             Text("Hello, world!")
@@ -22,23 +17,9 @@ struct ContentView: View {
                 Text("Hello, world!")
             }
             
-            Section {
-                Text("Hello, world!")
-                Text("Hello, world!")
-            }
-            
-            Section(header: Text("Personal Information")) {
-                TextField("Name", text: $name)
-                TextField("Email", text: $email)
-                Stepper(value: $age, in: 18...100) {
-                    Text("Age: \(age)")
-                }
-            }
-            
             Section(header: Text("Preferences")) {
-                Toggle(isOn: $receiveNewsletter) {
-                    Text("Receive Newsletter")
-                }
+                Text("Hello, world!")
+                Text("Hello, world!")
             }
         }
     }
