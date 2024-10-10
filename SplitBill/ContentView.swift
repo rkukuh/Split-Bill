e82ba737c78beb304b5863bc54e17b1017fbfaf2
R. Kukuh <rkukuh@gmail.com>
2024-10-11 03:50:12 +0700

    Step 1 (simplest)

--- SplitBill/ContentView.swift
+++ SplitBill/ContentView.swift
@@ -9,13 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Form {
+            ForEach(0..<100) { number in
+                Text("Row \(number)")
+            }
         }
-        .padding()
     }
 }
 
