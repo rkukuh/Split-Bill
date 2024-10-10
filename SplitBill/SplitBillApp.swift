8018a23463c9c97355ec16a20ccb0630ecdff8af
R. Kukuh <rkukuh@gmail.com>
2024-10-10 16:19:44 +0700

    Initial Commit

---
+++ SplitBill/SplitBillApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitBillApp.swift
+//  SplitBill
+//
+//  Created by R. Kukuh on 10/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SplitBillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
