2deca4fbf7944e1f3b63807801a904741054dd54
R. Kukuh <rkukuh@gmail.com>
2024-10-11 03:42:50 +0700

    Step 1 (expected error)

--- SplitBill/ContentView.swift
+++ SplitBill/ContentView.swift
@@ -8,14 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    var name = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Form {
+            TextField("Enter your name", text: name)
         }
-        .padding()
     }
 }
 
