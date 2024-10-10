f6b623b8ebdf48e7b5a21774e4c9590d950bd695
R. Kukuh <rkukuh@gmail.com>
2024-10-11 03:25:21 +0700

    Finish a chapter

--- SplitBill/ContentView.swift
+++ SplitBill/ContentView.swift
@@ -8,14 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var name: String = ""
+    @State private var email: String = ""
+    @State private var age: Int = 18
+    @State private var receiveNewsletter: Bool = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
+        Form {
             Text("Hello, world!")
+            Text("Hello, world!")
+            
+            Section {
+                Text("Hello, world!")
+            }
+            
+            Section {
+                Text("Hello, world!")
+                Text("Hello, world!")
+            }
+            
+            Section(header: Text("Personal Information")) {
+                TextField("Name", text: $name)
+                TextField("Email", text: $email)
+                Stepper(value: $age, in: 18...100) {
+                    Text("Age: \(age)")
+                }
+            }
+            
+            Section(header: Text("Preferences")) {
+                Toggle(isOn: $receiveNewsletter) {
+                    Text("Receive Newsletter")
+                }
+            }
         }
-        .padding()
     }
 }
 
