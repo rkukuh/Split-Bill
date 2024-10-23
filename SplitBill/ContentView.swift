d110ff309b2daa49eb88d8d9296252e37e1458ed
R. Kukuh <rkukuh@gmail.com>
2024-10-24 05:52:11 +0700

    Finish the chaptere

--- SplitBill/ContentView.swift
+++ SplitBill/ContentView.swift
@@ -12,6 +12,8 @@ struct ContentView: View {
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
     
+    @FocusState private var amountIsFocused: Bool
+    
     let tipPercentages = [10, 15, 20, 25, 0]
     
     var totalPerPerson: Double {
@@ -34,6 +36,7 @@ struct ContentView: View {
                               format: .currency(code: Locale.current.currency?.identifier ?? "USD")
                     )
                     .keyboardType(.decimalPad)
+                    .focused($amountIsFocused)
                     
                     Picker("Number of people", selection: $numberOfPeople) {
                         ForEach(2 ..< 11) { personCount in
@@ -60,6 +63,13 @@ struct ContentView: View {
                 }
             }
             .navigationTitle("SplitBill")
+            .toolbar {
+                if amountIsFocused {
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
         }
     }
 }
